f43b73d745df426daf9b283c1d2d1073ee19ad84
Gilmar Silva Santos Junior <gilmar.santos@jchebly.com.br>
2022-05-27 10:34:08 -0300

    adiciona validacao ao criar o campeonato

--- app/Http/Controllers/Team/TeamController.php
+++ app/Http/Controllers/Team/TeamController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers\Team;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Services\TeamServices;
-use Illuminate\Support\Facades\DB;
-use App\Repository\TeamRepository;
+use App\Models\Matches;
+use App\Models\Team;
 use App\Repository\MatchRepository;
 use App\Repository\PointRepository;
-use App\Models\Points;
-use App\Models\Team;
-use App\Models\Matches;
+use App\Repository\TeamRepository;
+use App\Services\TeamServices;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
@@ -22,7 +21,7 @@ class TeamController extends Controller
      */
     public function index()
     {
-       return view('main.home');
+        return view('main.home');
     }
 
     /**
@@ -35,9 +34,24 @@ class TeamController extends Controller
     //Deveria ter mantido toda lógica que trata os dados dos models direto nos models e desenvolver voltado para interface
 
     public function create(Request $request, TeamRepository $TeamRepository, MatchRepository $MatchRepository, PointRepository $PointsRepository)
-    {  
+    {
+        $validator = Validator::make($request->all(), [
+            'name1' => 'required',
+            'name2' => 'required',
+            'name3' => 'required',
+            'name4' => 'required',
+            'name5' => 'required',
+            'name6' => 'required',
+            'name7' => 'required',
+            'name8' => 'required',
+        ]);
+ 
+        if ($validator->fails()) {
+            return redirect('/')
+                        ->withErrors($validator);
+        }
 
-        
+
         $teams = [];
         $sort = new TeamServices();
         $goals = $sort->getGoals();
@@ -48,25 +62,28 @@ class TeamController extends Controller
         $group = array_pop($all_teams);
         $count = 0;
         shuffle($all_teams);
-        foreach($all_teams as $k=>$team){
-            $count ++;
+        foreach ($all_teams as $k => $team) {
+            $count++;
             $key = 'A';
-            if($count >4) $key = 'B';
+            if ($count > 4) {
+                $key = 'B';
+            }
+
             array_push($teams,
-            ["name"=>$team,
-             "key"=> $key,
-             "group"=>$group,
-        ]);
+                ["name" => $team,
+                    "key" => $key,
+                    "group" => $group,
+                ]);
         }
-        
+
         $TeamRepository->insert($teams);
         $times = $TeamRepository->select($group);
         $time_a = $sort->makeDisputeQF($times[0]['group_a']->toArray());
         $time_b = $sort->makeDisputeQF($times[0]['group_b']->toArray());
-        $teams_array =  array_merge($time_a, $time_b);
+        $teams_array = array_merge($time_a, $time_b);
         $match_a = [];
         $pts = [];
-        foreach($teams_array as $key => $match){
+        foreach ($teams_array as $key => $match) {
 
             $goals1 = $sort->getGoals()[0];
             $goals2 = $sort->getGoals()[1];
@@ -74,29 +91,29 @@ class TeamController extends Controller
             $points[] = $sort->getPoints($goals2, $goals1);
 
             array_push($match_a, [
-                "id_team1"=> $match[0]->id,
-                "goals_team1"=>$goals1,
-                "taken_team1" =>$goals2,
-                "points_team1"=>$goals1-$goals2,
-                "id_team2"=> $match[1]->id,
-                "goals_team2"=>$goals2,
-                "taken_team2" =>$goals1,
-                "points_team2"=> $goals2-$goals1,
-                "group"=> $group,
-                "match"=>'QF'
+                "id_team1" => $match[0]->id,
+                "goals_team1" => $goals1,
+                "taken_team1" => $goals2,
+                "points_team1" => $goals1 - $goals2,
+                "id_team2" => $match[1]->id,
+                "goals_team2" => $goals2,
+                "taken_team2" => $goals1,
+                "points_team2" => $goals2 - $goals1,
+                "group" => $group,
+                "match" => 'QF',
             ]);
-           array_push($pts, [
-               "id_team"=>$match[0]->id,
-               "points"=>$goals1-$goals2,
-               "key"=> $match[0]->key,
-               "match"=>'QF'
-           ]);
-           array_push($pts, [
-            "id_team"=>$match[1]->id,
-            "points"=>$goals2-$goals1,
-            "key"=> $match[1]->key,
-            "match"=>'QF'
-        ]);
+            array_push($pts, [
+                "id_team" => $match[0]->id,
+                "points" => $goals1 - $goals2,
+                "key" => $match[0]->key,
+                "match" => 'QF',
+            ]);
+            array_push($pts, [
+                "id_team" => $match[1]->id,
+                "points" => $goals2 - $goals1,
+                "key" => $match[1]->key,
+                "match" => 'QF',
+            ]);
         }
         //insere e trata as fases do campeonato
         $MatchRepository->insert($match_a);
@@ -127,46 +144,45 @@ class TeamController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show()
-    { 
+    {
         $m = new MatchRepository;
         $matches = Matches::all();
         $teams = Team::find(1);
         $data = [];
-        foreach($matches as $match){
+        foreach ($matches as $match) {
             array_push($data, [
-                "name"=>Team::find($match['id_team1'])['name'],
-                "group"=>Team::find($match['id_team1'])['group'],
-                "key"=>Team::find($match['id_team1'])['key'],
-                "goals"=>$match['goals_team1'],
-                "taken"=>$match['taken_team1'],
-                "points"=>$match['points_team1'],
-                "match"=>$match['match'],
+                "name" => Team::find($match['id_team1'])['name'],
+                "group" => Team::find($match['id_team1'])['group'],
+                "key" => Team::find($match['id_team1'])['key'],
+                "goals" => $match['goals_team1'],
+                "taken" => $match['taken_team1'],
+                "points" => $match['points_team1'],
+                "match" => $match['match'],
 
             ]);
             array_push($data, [
-                "name"=>Team::find($match['id_team2'])['name'],
-                "group"=>Team::find($match['id_team2'])['group'],
-                "key"=>Team::find($match['id_team2'])['key'],
-                "goals"=>$match['goals_team2'],
-                "taken"=>$match['taken_team2'],
-                "points"=>$match['points_team2'],
-                "match"=>$match['match'],
+                "name" => Team::find($match['id_team2'])['name'],
+                "group" => Team::find($match['id_team2'])['group'],
+                "key" => Team::find($match['id_team2'])['key'],
+                "goals" => $match['goals_team2'],
+                "taken" => $match['taken_team2'],
+                "points" => $match['points_team2'],
+                "match" => $match['match'],
 
             ]);
         }
-        
+
         $winner = $m->winner(Team::find($match['id_team2']));
 
-        if($winner[0]['points'] === $winner[1]['points']){
+        if ($winner[0]['points'] === $winner[1]['points']) {
             $winner = $winner[0];
             $menssagem = ': CRITÉRIO DE DESEMPATE';
-        }
-        else {
+        } else {
             $menssagem = '';
             $winner = $winner[0];
         }
         $winner = Team::find($winner['id_team']);
-        return view("main.championship", ["data"=>$data, "winner"=>$winner])->with('mensagem', $menssagem);
+        return view("main.championship", ["data" => $data, "winner" => $winner])->with('mensagem', $menssagem);
     }
 
     /**
